4e8f04139ef4faf3466b60075545068be17afef8
yuntongli <s2568943@gmail.com>
2020-12-03 17:11:31 +0800

    uid壞掉了啦

--- betResult.php
+++ betResult.php
@@ -4,7 +4,7 @@ if (! isset($_SESSION['account']) or $_SESSION['account']<="") {
 	header("Location: loginForm.php");
 } 
 require("dbconnect.php");
-$sql = "select record.id, record.period, record.price, record.num, lottery_list.bonus, lottery_list.ShowDate from record, lottery_list ;";
+$sql = "select record.id, record.period, record.price, record.num, lottery_list.bonus, lottery_list.ShowDate from record, lottery_list;";
 $result=mysqli_query($conn,$sql) or die("DB Error: Cannot retrieve message.");
 $rs=mysqli_fetch_assoc($result)
 
@@ -20,17 +20,17 @@ $rs=mysqli_fetch_assoc($result)
 <h1>下注表單</h1>
 <table id = bet width="300" border="5">
 <form method="post" action="resultControl.php">
-    <tr><td>下注編號</td><td> <?php echo $rs['id'] ?></td>
+    <tr><td id="id">下注編號</td><td> <?php echo $rs['id'] ?></td>
 
-    <tr><td>期數 : </td><td><?php echo $rs['period']?></tr>
+    <tr><td id="period">期數 : </td><td><?php echo $rs['period']?></tr>
     
-    <tr><td>開獎時間 :</td><td> <?php echo $rs['ShowDate']?></td></tr>
+    <tr><td id="showDate">開獎時間 :</td><td> <?php echo $rs['ShowDate']?></td></tr>
 
-    <tr><td>目前累積獎金 : </td><td><?php echo $rs['bonus']?></td></tr>
+    <tr><td id="bonus">目前累積獎金 : </td><td><?php echo $rs['bonus']?></td></tr>
 
-    <tr><td>下注號碼 : </td><td> <?php echo $rs['num'] ?></td></br>
+    <tr><td id="num">下注號碼 : </td><td> <?php echo $rs['num'] ?></td></br>
 
-    <tr><td>下注金額 : </td><td><?php echo $rs['price'] ?></td></tr></br>
+    <tr><td id="price">下注金額 : </td><td><?php echo $rs['price'] ?></td></tr></br>
 
     <button id = "home" onclick="location.href='firstview.php'">Home</button> 
 </form>
--- resultControl.php
+++ resultControl.php
@@ -1,16 +1,17 @@
 <?php
 require("dbconnect.php");
+$id=mysqli_real_escape_string($conn,$_POST['id']);
 $period=mysqli_real_escape_string($conn,$_POST['period']);
+$showdate=mysqli_real_escape_string($conn,$_POST['showDate']);
+$bonus=mysqli_real_escape_string($conn,$_POST['bonus']);
 $num=mysqli_real_escape_string($conn,$_POST['num']);
 $price=mysqli_real_escape_string($conn,$_POST['price']);
-$key=mysqli_real_escape_string($conn,$_POST['key']);
 
 if ($key) { //if title is not empty
-	$sql = "insert into record (period, num, price) values ('$period', '$num', '$price');";
+	$sql = "update lottery_list set lottery_list ;";
 	mysqli_query($conn, $sql) or die("Insert failed, SQL query error"); //執行SQL
 } else {
 	$msg = "key cannot be empty <br>";
 }
-header("Location:betResult.php");
 
 ?>
