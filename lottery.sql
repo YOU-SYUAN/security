e3c2552e00db094df3eb4b9bd680082b82bf0e4e
yuntongli <s2568943@gmail.com>
2020-12-20 19:17:15 +0800

    DB update

--- lottery.sql
+++ lottery.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2020-12-11 06:45:35
--- 伺服器版本： 10.4.13-MariaDB
--- PHP 版本： 7.4.8
+-- 產生時間： 
+-- 伺服器版本： 10.4.8-MariaDB
+-- PHP 版本： 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -42,8 +43,10 @@ CREATE TABLE `lottery_list` (
 --
 
 INSERT INTO `lottery_list` (`period`, `startTime`, `endTime`, `showDate`, `winNum`, `bonus`, `count`) VALUES
-(1, '2020-11-30 00:00:00.0', '2020-12-02 23:59:59.0', '2020-12-03 20:00:00.0', 33, 500, 0),
-(2, '2020-12-03 00:00:00.0', '2020-12-05 23:59:59.0', '2020-12-06 20:00:00.0', 20, 1000, 2);
+(1, '2020-11-30 00:00:00.0', '2020-12-02 23:59:59.0', '2020-12-03 20:00:00.0', 33, 1500, 0),
+(2, '2020-12-03 00:00:00.0', '2020-12-05 23:59:59.0', '2020-12-06 20:00:00.0', 20, 2600, 2),
+(3, '2020-12-17 00:00:00.0', '2020-12-19 23:59:59.0', '2020-12-20 18:00:00.0', 0, 3762, 0),
+(4, '2020-12-20 00:00:00.0', '2020-12-21 00:00:00.0', '2020-12-21 00:00:00.0', 0, 900, 0);
 
 -- --------------------------------------------------------
 
@@ -54,11 +57,12 @@ INSERT INTO `lottery_list` (`period`, `startTime`, `endTime`, `showDate`, `winNu
 CREATE TABLE `record` (
   `id` int(10) NOT NULL,
   `account` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `uid` int(10) NOT NULL,
   `period` int(10) NOT NULL,
   `num` int(10) NOT NULL,
   `price` int(10) NOT NULL,
-  `sign` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `salt` int(10) NOT NULL,
+  `sign` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `hash` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `status` int(2) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -66,35 +70,32 @@ CREATE TABLE `record` (
 -- 傾印資料表的資料 `record`
 --
 
-INSERT INTO `record` (`id`, `account`, `uid`, `period`, `num`, `price`, `sign`, `status`) VALUES
-(1, '', 1, 1, 10, 100, '', 0),
-(2, '', 0, 1, 11, 100, '', 0),
-(3, '', 0, 1, 15, 100, '', 0),
-(4, '', 0, 1, 12, 100, '', 0),
-(5, 'test', 0, 1, 10, 100, '', 0),
-(6, '', 0, 1, 14, 100, '', 0),
-(7, '', 0, 1, 16, 100, '', 0),
-(9, 'yo', 0, 1, 18, 200, '', 0),
-(10, '', 0, 1, 18, 100, '', 0),
-(11, '', 0, 1, 15, 100, '', 0),
-(12, '', 0, 1, 18, 100, '', 0),
-(13, '', 0, 1, 16, 100, '', 0),
-(16, '', 0, 1, 41, 100, '', 0),
-(17, '', 0, 1, 41, 100, '', 0),
-(18, '', 0, 1, 19, 100, '', 0),
-(19, '', 0, 1, 18, 200, '', 0),
-(22, '', 0, 1, 20, 100, '', 0),
-(23, '', 0, 1, 21, 100, '', 0),
-(24, '', 0, 1, 20, 100, '', 0),
-(25, '', 0, 1, 19, 100, '', 0),
-(27, 'yo', 0, 1, 20, 300, '', 0),
-(28, 'yo', 0, 1, 21, 300, '', 0),
-(29, 'yo', 0, 1, 17, 200, '', 0),
-(30, 'yo', 0, 0, 17, 300, '', 0),
-(31, 'yo', 0, 0, 19, 100, '', 0),
-(32, 'circle', 0, 0, 41, 200, '', 0),
-(33, 'yo', 0, 0, 18, 100, '', 0),
-(34, 'yo', 0, 0, 0, 100, '', 0);
+INSERT INTO `record` (`id`, `account`, `period`, `num`, `price`, `salt`, `sign`, `hash`, `status`) VALUES
+(1, 'test', 0, 15, 100, 0, '', '', 0),
+(2, 'test', 0, 15, 100, 0, '', '', 0),
+(3, 'test', 0, 15, 100, 0, '', '', 0),
+(4, 'test', 0, 16, 100, 0, '', '', 0),
+(5, 'test', 0, 16, 100, 0, '', '', 0),
+(6, 'test', 0, 16, 100, 0, '', '', 0),
+(7, 'test', 3, 1, 50, 0, '', '', 0),
+(8, 'test', 3, 41, 1000, 0, '', '', 0),
+(9, 'test', 3, 41, 1000, 0, '', '', 0),
+(10, 'test', 3, 34, 1000, 0, '', '', 0),
+(11, 'test', 3, 34, 10, 0, '', '', 0),
+(12, 'test', 3, 16, 100, 0, '', '', 0),
+(13, 'test', 3, 16, 100, 0, '', '', 0),
+(14, 'test', 3, 18, 100, 0, '', '', 0),
+(15, 'test', 3, 40, 100, 0, '', '', 0),
+(16, 'test', 3, 21, 1, 0, '', '', 0),
+(17, 'test', 3, 21, 1, 0, '', '', 0),
+(18, 'test', 3, 43, 100, 0, '', '', 0),
+(19, 'test', 3, 43, 100, 0, '', '', 0),
+(20, 'test', 3, 43, 100, 0, '', '', 0),
+(21, 'test', 4, 15, 100, 0, '', '', 0),
+(22, 'test', 4, 16, 100, 0, '', '', 0),
+(23, 'yo', 4, 16, 100, 0, '', '', 0),
+(24, 'yo', 4, 1, 500, 0, '', '', 1),
+(25, 'yo', 4, 9, 100, 805, '', '', 0);
 
 -- --------------------------------------------------------
 
@@ -156,13 +157,13 @@ ALTER TABLE `user`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `lottery_list`
 --
 ALTER TABLE `lottery_list`
-  MODIFY `period` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `period` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `record`
 --
 ALTER TABLE `record`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `user`
